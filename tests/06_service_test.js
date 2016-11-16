42738b764f84794014b869d042e1870137af72b7
ifeature <ifeature@yandex.ru>
2016-11-16 23:49:39 +0200

    :start2:

---
+++ tests/06_service_test.js
@@ -0,0 +1,14 @@
+﻿describe("Service Tests", function () {
+    beforeEach(angular.mock.module("exampleApp"));
+
+    let counterService;
+    beforeEach(angular.mock.inject(function(_counterService_) {
+      counterService = _counterService_;
+    }));
+
+    it("Increments the counter", function () {
+            expect(counterService.getCounter()).toEqual(0);
+            counterService.incrementCounter();
+            expect(counterService.getCounter()).toEqual(1);
+    });
+});
